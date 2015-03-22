69eaaec6e54dcfa9e5469fc4b4db8265864afde4
Oliver Batchelor <saulzar@gmail.com>
2015-03-23 00:20:33 +1300

    First working prototype

--- Game.elm
+++ Game.elm
@@ -13,28 +13,31 @@ import Maybe (..)
 import Signal
 
 import Model
-import Model (Point, Square, Model, SquareId, Dir(..))
+import Model (Point, Square, Model, SquareId, Dir(..), Coord)
 
 import Set (Set)
 import Set
 
 import Keyboard (KeyCode)
-import Time (..)
+import Keyboard
+import Time
+
+import Debug
+import String
   
   
 type alias KeyMap = Dict KeyCode Event
 type alias Interface = { model : Model, selected : Maybe SquareId,  keyMap : KeyMap }
-  
-  
-  
-  
-type Action = Select SquareId | KeyDown KeyCode
+
+
+type Action = Select SquareId | KeyDown KeyCode | Animate Int
 type Event = GoUp | GoLeft | GoRight | GoDown
   
   
 update : Action -> Interface -> Interface
-update action interface =
+update action interface = 
   case action of  
+    Animate dt -> animateModel dt interface
     Select sq -> select sq interface
     KeyDown key -> 
       maybe modelEvent 
@@ -43,10 +46,14 @@ update action interface =
         interface
       
       
+mapModel : (Model -> Model) -> Interface -> Interface
+mapModel f interface = {interface | model <- f interface.model}
+      
 modelEvent : Model.Event -> Interface -> Interface
-modelEvent event interface = { interface | model <- Model.event event interface.model }
+modelEvent event = mapModel (Model.event event)
 
-
+animateModel : Int -> Interface -> Interface
+animateModel dt = mapModel (Model.animate dt)
 
 updateEvent : Interface -> Event -> Maybe Model.Event
 updateEvent interface event = case event of
@@ -82,11 +89,22 @@ initial : Interface
 initial = { model = Model.initial, selected = Nothing, keyMap = defaultKeys }
 
 
-
+keyActions : Signal (Maybe Action)
+keyActions = Signal.map (\k -> Just (KeyDown k)) Keyboard.lastPressed
+                     
+animateActions : Signal (Maybe Action)
+animateActions = Signal.map (always (Just (Animate 1))) (Time.fps 50) 
+                     
+inputs : Signal (Maybe Action)
+inputs = Signal.mergeMany  [
+    keyActions, 
+    Signal.subscribe updates,
+    animateActions
+  ]
                      
 -- manage the model of our application over time
 model : Signal Interface
-model = Signal.foldp (maybe update) initial (Signal.subscribe updates)    
+model = Signal.foldp (maybe update) initial inputs
     
     
 -- updates from user input
@@ -99,10 +117,10 @@ sendUpdate action = Signal.send updates (Just action)
 
 
 view : Interface -> Html
-view interface =
-  svg [ version "1.1", x "0", y "0", viewBox "0 0 10 5" ]
-  (List.map (square interface) (Dict.toList interface.model.squares))
-    
+view interface = 
+  let squares = List.map (showSquare interface) (Dict.toList interface.model.squares)
+  in svg [ version "1.1", x "0", y "0", viewBox "0 0 20 10" ] squares
+  
     
 main = Signal.map view model  
 
@@ -111,16 +129,43 @@ squareColour : Interface -> SquareId -> String
 squareColour i s = let selectedCol = "#7FD13B"
                        defaultCol = "#4E9A06"
                    in if (i.selected == Just s) then selectedCol else defaultCol
-                    
-                    
-square : Interface -> (SquareId, Square) -> Html
-square i (k, sq) = rect
-      ([ fill (squareColour i k), width "1", height "1"
-      , transform "matrix(1 0 0 1 0 0)"
-      , onClick (sendUpdate (Select k))
-      ] ++ position sq.pos)
-      []    
 
-position : Point Int -> List Attribute
+
+
+
+catMaybes : List (Maybe a) -> List a
+catMaybes = List.filterMap identity
+
+showPivot : (Coord, Int) -> Html
+showPivot (pivot, _) = circle ([fill "#000000", r "0.1"] ++ centre pivot) []
+
+rotation : (Coord, Int) -> Attribute
+rotation (pivot, deg) = rotate deg pivot
+
+
+showSquare : Interface -> (SquareId, Square) -> Html
+showSquare i (k, sq) = 
+  let square = rectAt sq.pos 1 1
+        ([ fill (squareColour i k),
+          onClick (sendUpdate (Select k))
+        ] ++ catMaybes [rotation `Maybe.map` (Model.rotation sq)])
+        
+      pivot    = showPivot `Maybe.map` (Model.rotation sq)
+  in
+    g [] (catMaybes [Just square, pivot])
+    
+    
+rectAt : Coord -> Int -> Int -> List Attribute -> Html
+rectAt pos w h attrs = rect (width (toString w) :: height (toString h) :: position pos ++ attrs) []
+
+
+position : Point a -> List Attribute
 position p = [x (toString p.x), y (toString p.y)]
+      
+centre : Point a -> List Attribute
+centre p = [cx (toString p.x), cy (toString p.y)]
+
+rotate : a -> Point a -> Attribute
+rotate deg centre = transform (String.concat ["rotate (", toString deg, ",", toString centre.x, ",", toString centre.y, ")"])
+       
       
